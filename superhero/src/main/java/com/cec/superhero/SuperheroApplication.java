5b88bcdbfc859e4d6eda3833230ce705f8fcaece
Anthony Lam <lamanthony56@gmail.com>
2022-04-13 11:51:41 -0700

    fixed project errors

--- superhero/src/main/java/com/cec/superhero/SuperheroApplication.java
+++ superhero/src/main/java/com/cec/superhero/SuperheroApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 //@EntityScan("")
 @SpringBootApplication
 public class SuperheroApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SuperheroApplication.class, args);
 	}
-
 }
